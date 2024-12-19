2ed5023a1571b33a0d8b3d1d6a7e375f40b36e7c
Nguyen Van Vu <nguyenvanvu028@gmail.com>
2024-12-20 06:42:03 +0700

    update

--- init.lua
+++ init.lua
@@ -13,19 +13,14 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
-
--- Make line numbers default
 vim.opt.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
-vim.opt.relativenumber = true
+vim.opt.relativenumber = false
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -41,7 +36,6 @@ vim.schedule(function()
   vim.opt.clipboard = 'unnamedplus'
 end)
 
--- Enable break indent
 vim.opt.breakindent = true
 
 -- Save undo history
@@ -860,20 +854,17 @@ require('lazy').setup({
   },
 
   {
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+    -- `:Telescope colorscheme`.
     -- 'folke/tokyonight.nvim',
-    'navarasu/onedark.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
+    -- 'navarasu/onedark.nvim',
+    'sainnhe/gruvbox-material',
+    priority = 1000,
     init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'onedark'
+      vim.g.gruvbox_material_enable_italic = true
+      -- vim.g.gruvbox_material_dim_inactive_windows = true
+      vim.g.gruvbox_material_background = 'medium'
+      vim.cmd.colorscheme 'gruvbox-material'
 
-      -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
   },
